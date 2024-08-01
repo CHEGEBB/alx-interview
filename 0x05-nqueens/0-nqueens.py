2399e323a796743619c5802bf9dd991ba8c5afc2
CHEGEBB <chegephil24@gmail.com>
2024-08-01 22:29:15 +0300

    new

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col, n):
+
+def is_safe(board, row, col):
     # Check this row on left side
     for i in range(col):
         if board[row][i] == 1:
@@ -13,44 +14,37 @@ def is_safe(board, row, col, n):
             return False
 
     # Check lower diagonal on left side
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
 
-def solve_nqueens(board, col, n, solutions):
-    if col >= n:
+
+def solve_nqueens(board, col):
+    if col >= len(board):
         solution = []
-        for i in range(n):
-            for j in range(n):
+        for i in range(len(board)):
+            for j in range(len(board)):
                 if board[i][j] == 1:
                     solution.append([i, j])
-        solutions.append(solution)
-        return
+        print(solution)
+        return True
 
-    for i in range(n):
-        if is_safe(board, i, col, n):
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
             board[i][col] = 1
-            solve_nqueens(board, col + 1, n, solutions)
+            res = solve_nqueens(board, col + 1) or res
             board[i][col] = 0
 
-def print_solutions(solutions):
-    for solution in solutions:
-        print(solution)
+    return res
+
 
 def nqueens(n):
-    if not isinstance(n, int):
-        print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
     board = [[0 for _ in range(n)] for _ in range(n)]
-    solutions = []
-    solve_nqueens(board, 0, n, solutions)
-    print_solutions(solutions)
+    solve_nqueens(board, 0)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -59,7 +53,12 @@ if __name__ == "__main__":
 
     try:
         N = int(sys.argv[1])
-        nqueens(N)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    nqueens(N)
